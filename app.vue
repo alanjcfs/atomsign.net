0e296ab89016c5446d597b6481f4037b24e91681
Alan Fung-Schwarz <alan@atomsign.net>
2023-11-05 16:33:39 -0800

    Get Nuxt set up

--- app.vue
+++ app.vue
@@ -3,11 +3,14 @@
     <UCard class="mt-10">
       <template #header>
         <div class="flex justify-between">
-          <h1>Welcome to Nuxt UI Starter</h1>
+          <router-link to="/"><h1 class="text-2xl font-bold">Home</h1></router-link>
+          <router-link to="/about"><h1 class="text-2xl font-bold">About</h1></router-link>
           <ColorScheme><USelect v-model="$colorMode.preference" :options="['system', 'light', 'dark']" /></ColorScheme>
         </div>
       </template>
-      <UButton icon="i-heroicons-book-open" to="https://ui.nuxt.com" target="_blank">Open Nuxt UI Documentation</UButton>
+    </UCard>
+    <UCard class="mt-10">
+      <NuxtPage />
     </UCard>
   </UContainer>
 </template>
