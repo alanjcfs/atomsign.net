b8c9301b403c6a4bfe509d403ca1f2ab90c3417a
Alan Fung-Schwarz <alan@atomsign.net>
2023-11-05 17:11:11 -0800

    Remove About

--- app.vue
+++ app.vue
@@ -4,7 +4,6 @@
       <template #header>
         <div class="flex justify-between">
           <router-link to="/"><h1 class="text-2xl font-bold">Home</h1></router-link>
-          <router-link to="/about"><h1 class="text-2xl font-bold">About</h1></router-link>
           <ColorScheme><USelect v-model="$colorMode.preference" :options="['system', 'light', 'dark']" /></ColorScheme>
         </div>
       </template>
