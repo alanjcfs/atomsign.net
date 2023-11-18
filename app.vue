2a10a69123b029bfc74387899eb34b115129eebb
Alan Fung-Schwarz <alan@atomsign.net>
2023-11-18 13:28:57 -0800

    Show Code as external

--- app.vue
+++ app.vue
@@ -3,21 +3,26 @@
     <UCard class="mt-10">
       <template #header>
         <div class="flex justify-between">
-          <nuxt-link
+          <NuxtLink
             class="text-4xl font-bold"
             to="/"
-          >Atom.</nuxt-link>
+          >Atom.</NuxtLink>
 
           <a
             class="text-4xl font-bold no-underline"
-            href="//github.com/alanjcfs"
-          >Code.</a>
+            href="https://github.com/alanjcfs"
+          >
+            Code.
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 inline align-baseline">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 6H5.25A2.25 2.25 0 003 8.25v10.5A2.25 2.25 0 005.25 21h10.5A2.25 2.25 0 0018 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25" />
+              </svg>
+          </a>
 
-          <nuxt-link
+          <NuxtLink
             class="text-4xl font-bold no-underline"
             to="/american-sign-language">
             Sign.
-          </nuxt-link>
+          </NuxtLink>
           <ColorScheme>
             <USelect
               v-model="$colorMode.preference"
