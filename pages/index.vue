acc102d87dd7e970a8f0e637ab43ff6f5d8f3ebc
Alan Fung-Schwarz <alan@atomsign.net>
2023-12-18 13:59:32 -0800

    Add nuxt link

---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <main>
+    <ContentList path="/2023" v-slot="{ list }">
+      <ul class="list-none">
+        <li v-for="article in list" :key="article._path">
+          <nuxt-link :to="article.path">{{ article.title }}</nuxt-link>
+        </li>
+      </ul>
+    </ContentList>
+  </main>
+</template>
